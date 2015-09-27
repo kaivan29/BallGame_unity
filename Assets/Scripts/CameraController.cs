708b8872cfbfc6eccdabd32caee642d45333d81e
Kaivan Shah <kaivan29@tamu.edu>
2015-09-27 09:11:04 -0500

    Ball game unity

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject player;
+	
+	private Vector3 offset;
+	
+	void Start()
+	{
+		offset = transform.position - player.transform.position;
+		//the camera needs to move along with the object. Inertia!!
+		//We have to stay true to the frame. Hence, the difference
+		//our transform.position - camera position
+	}
+
+	void LateUpdate()
+	{
+		transform.position = offset + player.transform.position;
+		//the camera is moved to the new position now.
+		//So we need to update the position of the camera
+		//For follow camera it is better to use LateUpdate rather than Update.
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15,30,45)*Time.deltaTime);
+		//public void Rotate(Vector3 eulerAngles, Space relativeTo = Space.Self); 
+		//15, 45 and 30 are euler angles
+		//we are going to keep space default here.
+	}
+
+	//A prefab is an asset that contains a template
+}
